54702b1b3b0ca19b827f77debdc2676bee36f615
Мария Грешникова <GreshnikovaMA@mpei.ru>
2018-03-20 11:14:05 +0300

    Решение конфликтов

--- main.cpp
+++ main.cpp
@@ -8,5 +8,7 @@ int main()
    cout <<"Enter A and B: ";
    cin>>a>>b;
    cout<<"A+B="<<a+b<<'\n';
-   cout<<"2A="<<2*a<<'\n'
+   cout<<"2A="<<2*a<<'\n';
+   cout<<"A-B"<<a-b<<'\n';
+   cout<<"A*B"<<a*b<<'\n';
 }
